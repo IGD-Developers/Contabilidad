a549211b555ed4567f7f9a499e6595ac9b7d25b3
Ts-Pytham <43942761+Ts-Pytham@users.noreply.github.com>
2023-02-04 18:40:15 -0500

    Eliminación de estructura anidada en Configuración -> Sucursal y cambios de algunos archivos.

--- WebAPI/Aplicacion/Configuracion/Sucursales/ConsultaId.cs
+++ WebAPI/Aplicacion/Configuracion/Sucursales/ConsultaId.cs
@@ -10,46 +10,42 @@ using ContabilidadWebAPI.Dominio.Configuracion;
 
 namespace ContabilidadWebAPI.Aplicacion.Configuracion.Sucursales;
 
-public class ConsultaId
+public class ConsultarSucursalRequest : IdSucursalModel, IRequest<ListarSucursalModel>
+{ }
+
+public class ConsultarSucursalHandler : IRequestHandler<ConsultarSucursalRequest, ListarSucursalModel>
 {
-    public class ConsultarId : IdSucursalModel, IRequest<ListarSucursalModel>
-    { }
 
-    public class Manejador : IRequestHandler<ConsultarId, ListarSucursalModel>
+    private CntContext _context;
+
+    private readonly IMapper _mapper;
+
+
+
+    public ConsultarSucursalHandler(CntContext context, IMapper mapper)
     {
+        _context = context;
+        _mapper = mapper;
 
-        private CntContext _context;
-
-        private readonly IMapper _mapper;
-
-
-
-        public Manejador(CntContext context, IMapper mapper)
-        {
-            _context = context;
-            _mapper = mapper;
-
-        }
-
-
-        public async Task<ListarSucursalModel> Handle(ConsultarId request, CancellationToken cancellationToken)
-        {
-
-            var entidad = await _context.cnfSucursales
-            .Include(e => e.Empresa)
-            .SingleOrDefaultAsync(i => i.Id == request.Id);
-
-
-            if (entidad == null)
-            {
-                throw new Exception("Registro no encontrado");
-            };
-            var entidadDto = _mapper.Map<CnfSucursal, ListarSucursalModel>(entidad);
-
-            return entidadDto;
-
-
-        }
     }
 
+
+    public async Task<ListarSucursalModel> Handle(ConsultarSucursalRequest request, CancellationToken cancellationToken)
+    {
+
+        var entidad = await _context.cnfSucursales
+        .Include(e => e.Empresa)
+        .SingleOrDefaultAsync(i => i.Id == request.Id);
+
+
+        if (entidad == null)
+        {
+            throw new Exception("Registro no encontrado");
+        };
+        var entidadDto = _mapper.Map<CnfSucursal, ListarSucursalModel>(entidad);
+
+        return entidadDto;
+
+
+    }
 }
